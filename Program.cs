d7578409a6f8d098f2e7d5ced32e3ca9eb195fc2
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-03-15 21:32:56 -0300

    finalizado

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿
+Console.WriteLine("Vida Vazia");
+
+Console.WriteLine(@"Depois que você foi embora
+A solidão entrou, trancou a porta
+E não me deixa mais
+Eu já tentei sair
+Tentei fugir, não consegui
+Eu já não tenho paz
+Até o meu sorriso é tão sem graça
+Não há nada que desfaça
+Essa tristeza em meu olhar
+O que é que eu vou fazer pra te esquecer?
+O que é que eu vou fazer pra não sofrer?
+O que é que eu faço pra você voltar
+Pra minha vida?
+Vida vazia, saudade sua
+Dia nublado, vento gelado
+Noite sem lua
+Vida vazia de sentimento
+Noite sem sono, no abandono
+Eu não aguento
+Até o meu sorriso é tão sem graça
+Não há nada que disfarça
+Essa tristeza em meu olhar
+O que é que eu vou fazer pra te esquecer?
+O que é que eu vou fazer pra não sofrer?
+O que é que eu faço pra você voltar
+Pra minha vida?
+Vida vazia, saudade sua (vazia)
+Dia nublado, vento gelado
+Noite sem lua (uh uh uh)
+Vida vazia de sentimento (vazia)
+Noite sem sono, no abandono
+Eu não aguento (uh uh uh)
+Vida vazia, oh oh oh (vazia)
+Dia nublado, vento gelado
+Noite sem lua (uh uh uh)
+Vida vazia de sentimento (vazia)
+Noite sem sono, no abandono
+Eu não aguento (uh uh uh)
+Vida vazia");
